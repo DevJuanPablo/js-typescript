dbf1779745510c28bba40eeea592c2f6b1447fa3
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-11 22:04:37 -0400

    probando el ...spread operator como parametro de una funcion

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-type AUTENTICADO = {
-  jwt: string
-  userId: string
-  role: string
-  level?: number
+type ARGS = number | string | boolean
+
+const argumentos = (...param: ARGS[]): ARGS[] => {
+    
+    return param
 }
 
-const estaAutenticado = (jwt: string, userId: string = 'admin', role: string = 'customer', level?: number): AUTENTICADO => {
-  return {
-    userId,
-    jwt,
-    role,
-    level: level ?? 10
-  }
-}
-
-const respuesta = estaAutenticado('bnbmw688')
-console.log(respuesta)
-
-const respuesta2 = estaAutenticado('bnkia360 ', 'chavo', 'admin')
-console.log(respuesta2) 
-
-const respuesta3 = estaAutenticado('bnfrri124', 'adrian')
-console.log(respuesta3)
+const resultado = argumentos(1, 2, 3, "Hola", true, false, "Adios")
+console.log(resultado) // [ 1, 2, 3, 'Hola', true, false, 'Adios' ] 
