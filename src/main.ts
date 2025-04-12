96b69fc9dbee3d1be5f8e631ce7cdab6195f0ea8
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-11 22:41:52 -0400

    probando la herencia con interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
-interface Usuario {
+interface Base {
+  id: string,
+  address?: string,
+  email?: string,
+}
+
+interface Usuario extends Base{
   nombre: string,
   edad: number,
   ciudad: string,
 }
 
+interface Persona extends Base{
+  phone: number
+}
+
 const usuario: Usuario = {
+  id: "123",
   nombre: "Juan",
   edad: 30,
   ciudad: "Madrid",
+}
+
+const persona: Persona = {
+  id: "456",
+  address: "Calle Falsa 123",
+  phone: 123456789,
+  email: "miemail@mail.com"
 }
 
 console.log(usuario.edad)
