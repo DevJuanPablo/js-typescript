1a8a66e4f73801bba983679abc65655c6566b624
Dev Juan Pablo <dev.juanpablo.ortiz@gmail.com>
2025-04-11 22:10:06 -0400

    probando el ...spread operator y objetos como parametro de una funcion

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 type ARGS = number | string | boolean
 
-const argumentos = (...param: ARGS[]): ARGS[] => {
+const argumentos = (...param: any[]): any[] => {
     
     return param
 }
 
+const usuario = {
+  name: 'Adrian',
+  email: 'correo@dominio.com',
+  edad: 30
+}
+
 const resultado = argumentos(1, 2, 3, "Hola", true, false, "Adios")
 console.log(resultado) // [ 1, 2, 3, 'Hola', true, false, 'Adios' ] 
+
+const resultado2 = argumentos(usuario)
+console.log(resultado2)
